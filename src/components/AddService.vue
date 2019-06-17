32e6688b610047386e242baa3fe167f508170fc3
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-06-16 20:34:56 -0400

    docker setup

---
+++ src/components/AddService.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="add-service">+</div>
+</template>
+
+<script>
+export default {
+  name: "AddService"
+};
+</script>
+
+<style scoped>
+.add-service {
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  right: 16px;
+  bottom: 16px;
+  background-color: #024c8e;
+  height: 64px;
+  width: 64px;
+  border-radius: 100%;
+  font-size: 2.6em;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/Subscription.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="subscription" :style="`background: ${color}`">
+    <h1>{{title}}</h1>
+
+    <div class="details">
+      <span>${{price}}</span>
+      <span class="delete">X</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Subscription",
+  props: {
+    title: String,
+    price: Number,
+    color: String
+  }
+};
+</script>
+
+<style scoped>
+.subscription {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 24px;
+  border-radius: 10px;
+  padding: 0 16px;
+}
+
+.details {
+  align-self: center;
+  font-size: 1.2em;
+  font-weight: bold;
+}
+
+.delete {
+  margin-left: 8px;
+}
+</style>
+
+
