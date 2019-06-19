b1505cd548aaebd82903d4f9e8d694c44f2f2226
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-06-18 22:35:10 -0400

    add subscription page template

---
+++ src/views/AddPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="addPage">
+    Enter Subscription
+    <form action="post">
+      <input type="text" placeholder="Name">
+      <input type="number" name="price" id="price" min="1" step="1">
+      <!-- insert datepicker library -->
+
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddPage"
+};
+</script>
+
+<style scoped>
+.addPage {
+  text-align: center;
+}
+</style>
